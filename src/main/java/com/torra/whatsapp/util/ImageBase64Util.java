ede41680da936e57362fbc9b929285b99b2459cb
Caio Souza <caioss20.cs@gmail.com>
2026-03-19 18:43:17 -0300

    feat: new class converteImage to base64

--- src/main/java/com/torra/whatsapp/util/ImageBase64Util.java
+++ src/main/java/com/torra/whatsapp/util/ImageBase64Util.java
@@ -1,5 +1,22 @@
-package main.java.com.torra.whatsapp.util;
+package com.torra.whatsapp.util;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
 
 public class ImageBase64Util {
-    
+
+    private ImageBase64Util() {
+    }
+
+    public static String toBase64png(BufferedImage image) throws IOException {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ImageIO.write(image, "png", outputStream);
+
+         String base64 = Base64.getEncoder().encodeToString(outputStream.toByteArray());
+        return "data:image/png;base64," + base64;
+    }
 }
