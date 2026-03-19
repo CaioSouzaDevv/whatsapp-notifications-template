c27da92eaf6ba4b99970fd733cc2d4bac36b1cf1
Caio Souza <caioss20.cs@gmail.com>
2026-03-19 18:33:23 -0300

    feat: including config api

--- src/main/java/com/torra/whatsapp/config/ZApiConfig.java
+++ src/main/java/com/torra/whatsapp/config/ZApiConfig.java
@@ -1,5 +1,31 @@
-package main.java.com.torra.whatsapp.config;
+package com.torra.whatsapp.config;
 
 public class ZApiConfig {
-    
+    private final String instanceId;
+    private final String token;
+    private final String clientToken;
+    private final String phone;
+
+    public ZApiConfig(String instanceId, String token, String clientToken, String phone) {
+        this.instanceId = instanceId;
+        this.token = token;
+        this.clientToken = clientToken;
+        this.phone = phone;
+    }
+
+    public String getInstanceId() {
+        return instanceId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getClientToken() {
+        return clientToken;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
 }
