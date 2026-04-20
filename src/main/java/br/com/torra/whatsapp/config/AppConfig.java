bb2c7e9aac244c7338f09959c953ba449bbc5f18
Caio de Souza Silva <caio.silva@lojastorra.com.br>
2026-04-20 16:46:29 -0300

    style: ajustando arrows na imagem

--- src/main/java/br/com/torra/whatsapp/config/AppConfig.java
+++ src/main/java/br/com/torra/whatsapp/config/AppConfig.java
@@ -8,7 +8,7 @@ public class AppConfig {
 
     public static final String META_API_VERSION = "v25.0";
     public static final String META_PHONE_NUMBER_ID = "1157240740795278";
-    public static final String META_ACCESS_TOKEN = "EAApo24Aj0C4BRCLqFiVC6H3xr2tQ6DW8rp4AUMCFdWSodIkMfViOEcVzQkpmNamHtGorrAMDzUc5jPZCZB6i2fSqEZBqe8HjR1Gssr7903JoDZCxnmpmUsFQymhTMjXphP1ITSxrT1cqZCZAmOdhXFyn9FFZB9L3QRYnP2TNwmYHdE3aF7XLYN6BB6qvazKLnE51mXP2ile6RZCW9R6lZASlkxNx6xNDlCE7RsYcbSirVhaGZC7O4aWagJFuDM16f5x7LE1HHHQIMfLSRzKwAyan9JL9Ke";
+    public static final String META_ACCESS_TOKEN = "EAApo24Aj0C4BRREuP1dL5CWrtZA2hhf1ZAn6e0CRcIgSyA9iwq9zo7cxvEkpZCSPBn0SCQj3R1qGfPFOJXfEgvPykZBxxLV8ZAitZCQZAZA6k6qyMD9A3tRVRtv4XCdfAoHrtzVKqYJhRowLP1EzeTCMjN0E6wxvsRpFaBOB9H8Pst67mcUtJZCoSLzcgzfaLbRIzugOrVtwlvuom6XJiZCXPcVDMFGtDtQq8Xuf9jfmlbh5QIeo08GvxlgBFE6B5RulpHfJ0AhoAcXrrGCSmVBvnsfhBZA";
     // public static final String META_TEST_TO_PHONE = "5511984696489";
     public static final String META_TEMPLATE_NAME = "hello_world";
     public static final String META_TEMPLATE_LANGUAGE = "en_US";
